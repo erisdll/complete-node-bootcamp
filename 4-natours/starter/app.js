2c9a30d2e7cc253c3f98b043064d42fd47a85a35
nearlyerika <72031082+nearlyerika@users.noreply.github.com>
2023-06-02 11:47:06 -0300

    mock API - get & patch done

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
+app.use(express.json);
+
 // app.get('/', (req, res) => {
 //   res
 //     .status(200)
@@ -13,16 +15,60 @@ const app = express();
 //   res.send('Yuo can post to this endpoint');
 // });
 
-const tours = fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`);
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
 app.get('api/v1/tours', (req, res) => {
   res.status(200).json({
-    status: "success",
+    status: 'success',
     results: tours.length,
     data: {
-      tours
+      tours,
+    },
+  });
+});
+
+app.get('api/v1/tours/:id/', (req, res) => {
+  console.log(req.params)
+  const id = +req.params.id;
+  
+  const tour = tours.find((el) => el.id === id);
+
+  // if (id > tour.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: 'Fail',
+      message: 'Invalid ID',
+    });
+  }
+
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post('api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'Success!',
+        data: {
+          newTour,
+        },
+      });
     }
-  })
+  );
 });
 
 const PORT = 3000;
