35747086d01d97917a0d81a7223b052da42018b1
nearlyerika <72031082+nearlyerika@users.noreply.github.com>
2023-06-06 07:15:56 -0300

    doing middleware

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -9,13 +9,21 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString()
+  next()
+})
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
 );
 
 const getALLTours = (req, res) => {
+  console.log(req.requestTime)
+
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours,
