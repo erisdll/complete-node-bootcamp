5648926ddfc3d945c017ea8502efeff67b5258dd
nearlyerika <72031082+nearlyerika@users.noreply.github.com>
2023-06-13 13:04:48 -0300

    .env configured

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -6,7 +6,10 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // MIDDLEWARES
-app.use(morgan('dev'))
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`))
 
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,6 +1,9 @@
-const app = require('./app')
+const dotenv = require('dotenv')
+dotenv.config({ path: './.env' })
 
-const PORT = 3000;
+const app = require('./app')
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}...`);
 });
