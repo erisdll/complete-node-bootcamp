55589ff2c5517b8f6dbc54f279063a0a572743a2
nearlyerika <72031082+nearlyerika@users.noreply.github.com>
2023-08-15 14:49:44 -0300

    implemented basic centralized error handling

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,30 +1,34 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const tourRouter = require('./routes/tourRoutes')
+const AppError = require('./utils/appError')
+const globalErrorHandler = require('./controllers/errorController')
+const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 // MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'))
+  app.use(morgan('dev'));
 }
 
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`))
-
-app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
-});
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
+// ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-module.exports = app
+app.all('*', (req, res, next) => {
+  next(new AppError(`Cant find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
