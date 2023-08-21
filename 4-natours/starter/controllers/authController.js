be208efb67113d767452102778f04362e8c860db
nearlyerika <72031082+nearlyerika@users.noreply.github.com>
2023-08-21 11:56:28 -0300

    user login done

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -1,6 +1,12 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/userModel');
-const catchAsync = require('./../utils/catchAsync');
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
+const signToken = id => {
+  return jwt.sign({id}, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+}
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -9,10 +15,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
   });
-
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN
-  });
+  const token = signToken(newUser._id)
 
   res.status(201).json({
     status: 'success',
@@ -23,6 +26,21 @@ exports.signup = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.login = (req, res, next) => {
-  const {email, password} = req.body
-}
+exports.login = catchAsync(async (req, res, next) => {
+  const {email, password} = req.body;
+
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password!', 400));
+  }
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await User.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401))
+  }
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: 'success',
+    token
+  });
+});
