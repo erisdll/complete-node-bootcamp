261f053159d298a8c6f8f00f38e6f0bb87846242
nearlyerika <72031082+nearlyerika@users.noreply.github.com>
2023-08-27 16:47:36 -0300

    updated routes

--- 4-natours/starter/routes/userRoutes.js
+++ 4-natours/starter/routes/userRoutes.js
@@ -7,8 +7,14 @@ const router = express.Router();
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 
-router.post('/forgotPassword', authController.signup);
-router.post('/resetPassword', authController.login);
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+router.patch(
+  '/updateMyPassword',
+  authController.protect,
+  authController.updatePassword,
+);
+
 
 router
   .route('/')
