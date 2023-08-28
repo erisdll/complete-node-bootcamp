5220473cb2a24eb9533cbbb695c0ce38a9532f81
nearlyerika <72031082+nearlyerika@users.noreply.github.com>
2023-08-28 16:51:36 -0300

    implemented updateMe & deleteMe

--- 4-natours/starter/controllers/userController.js
+++ 4-natours/starter/controllers/userController.js
@@ -2,6 +2,14 @@ const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if(allowedFields.includes(el)) newObj[el] = obj[el]
+  })
+  return newObj;
+}
+
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
 
@@ -13,6 +21,43 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates! Please use /updateMyPassword',
+        400,
+      ),
+    );
+  }
+
+  // 2) Filter out unwanted field names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 2) Update user document
+  const updatedUser = await User.findByIdandUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      updatedUser
+    },
+  });
+})
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdandUpdate(req.user.id, { active: false })
+  
+  req.status(204).json({
+    status: 'success',
+    data: null
+  });
+})
 
 exports.getUser = (req, res, next) => {
   res.status(500).json({
--- 4-natours/starter/models/userModel.js
+++ 4-natours/starter/models/userModel.js
@@ -40,6 +40,11 @@ const userSchema = new mongoose.Schema({
   passwordChangedAt: Date,
   passwordResetToken: String,
   passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  }
 });
 
 userSchema.pre('save', async function (next) {
@@ -52,11 +57,17 @@ userSchema.pre('save', async function (next) {
 });
 
 userSchema.pre('save', function (next) {
-  if (!this.isModified('password')|| this.isNew) return next();
+  if (!this.isModified('password') || this.isNew) return next();
   
   this.passwordChangedAt = Date.now - 1000;
   next();
-})
+});
+
+userSchema.pre(/^find/, function (next) {
+  // This points to the current query
+  this.find({ active: {$ne: false} })
+  next()
+});
 
 userSchema.methods.correctPassword = async function (
   cadidatePassword,
@@ -72,7 +83,7 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
     return JWTTimestamp < changedTimestamp
   }
   return false
-}
+};
 
 userSchema.methods.createPasswordResetToken = function () {
   const resetToken = crypto.randomBytes(32).toString('hex');
