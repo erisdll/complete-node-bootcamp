6cf5e4024d24665fe35a04f0d8b0df456f899cca
nearlyerika <72031082+nearlyerika@users.noreply.github.com>
2023-08-29 16:48:39 -0300

    implemented cookie send

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -13,7 +13,23 @@ const signToken = (id) => {
 };
 
 const createSendToken = (user, statusCode, res) => {
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 3600 * 1000,
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    cookieOptions.secure = true;
+  }
+
   const token = signToken(user._id);
+
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove password fomr output
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: 'success',
