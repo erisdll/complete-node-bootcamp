0d6096b56218e021c684ae98710329d8bc59462c
nearlyerika <72031082+nearlyerika@users.noreply.github.com>
2023-10-06 16:30:47 -0300

    added fields

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify')
+//const User = require('./userModel')
 
 const tourSchema = new mongoose.Schema(
   {
@@ -75,6 +77,36 @@ const tourSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    startLocation: {
+      // GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point'],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point'],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
+    guide: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User'
+      }
+    ]
   },
   {
     toJSON: {virtuals: true},
@@ -92,6 +124,12 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
+// tourSchema.pre('save', async function (next) {
+//   const guidesPromises = this.guides.map(async id => await User.findById(id))
+//   this.guids = await Promise.all(guidesPromises)
+//   next();
+// });
+
 // tourSchema.pre('save', function (next) {
 //   console.log('Will save document!')
 //   next();
@@ -108,6 +146,14 @@ tourSchema.pre(/^find/, function (next) {
   this.start = Date.now();
   next();
 });
+
+tourSchema.pre(/^find/, function (next) {
+  this.polulate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt'
+  });
+  next();
+})
 
 tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
