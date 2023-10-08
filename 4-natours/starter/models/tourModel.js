260bc9ac6bac513fafc4006f0d1996ec5c07d000
erisdll <72031082+erisdll@users.noreply.github.com>
2023-10-08 20:11:43 -0300

    implemented review populate into tour model

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -118,6 +118,13 @@ tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
 
+// Vitual Populate
+tourSchema.virtual('reviews', {
+  ref: 'Review',
+  foreignField: 'tour',
+  localField: '_id'
+});
+
 // DOCUMENT MIDDLEWARE: runs before the .save() and .create()
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, {lower: true});
