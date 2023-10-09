7e5b0914404a47a7460396cddcbd4436e0cab65b
nearlyerika <72031082+nearlyerika@users.noreply.github.com>
2023-10-09 16:37:01 -0300

    updated routes

--- 4-natours/starter/routes/userRoutes.js
+++ 4-natours/starter/routes/userRoutes.js
@@ -9,6 +9,7 @@ router.post('/login', authController.login);
 
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
+
 router.patch(
   '/updateMyPassword',
   authController.protect,
