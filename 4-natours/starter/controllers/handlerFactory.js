a5e6747fca66287d882b2ab905f7dc9c4a868fc5
nearlyerika <72031082+nearlyerika@users.noreply.github.com>
2023-10-09 16:36:18 -0300

    updated handler factory

--- 4-natours/starter/controllers/handlerFactory.js
+++ 4-natours/starter/controllers/handlerFactory.js
@@ -1,6 +1,77 @@
-const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
+const APIFeatures = require('./../utils/apiFeatures');
 
+exports.createOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.create();
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    // To allows for nested getReviews on tours ('hack')
+    let filter = {};
+    if (req.params.tourId) filter = {tour: req.params.tourId};
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const doc = await features.query;
+    // SEND RESPONSE
+    res.status(200).json({
+      status: 'success',
+      results: doc.length,
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.getOne = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+    if(popOptions) query = query.populate(popOptions)
+    const doc = await query;
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID!', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        doc,
+      },
+    });
+  });
+  
+exports.updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID!', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+  
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
     const doc = await Model.findByIdAndDelete(req.params.id);
@@ -12,24 +83,5 @@ exports.deleteOne = (Model) =>
     res.status(204).json({
       status: 'success',
       data: null,
-    });
-  });
-
-exports.updateOne = (Model) =>
-  catchAsync(async (req, res, next) => {
-    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-
-    if (!doc) {
-      return next(new AppError('No tour found with that ID!', 404));
-    }
-
-    res.status(200).json({
-      status: 'success',
-      data: {
-        data: doc
-      },
     });
   });
