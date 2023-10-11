f0910f5c054e2b1c98cd7708cba54f9130b6247b
nearlyerika <72031082+nearlyerika@users.noreply.github.com>
2023-10-11 15:06:41 -0300

    updated models

--- 4-natours/starter/models/reviewModel.js
+++ 4-natours/starter/models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -37,7 +38,33 @@ reviewSchema.pre(/^find/, function (next) {
     path: 'user',
     select: 'name photo'
   })
-})
+});
+
+reviewSchema.statics.calcAvarageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId }
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' }
+      }
+    }
+  ]);
+  
+  Tour.findByIdAndUpdate(tourId, {
+    ratingsQuantity: stats[0].nRating,
+    ratingsAvarage: stats[0].avgRating,
+  });
+}
+
+reviewSchema.post('save', function (next) {
+  // Points to currently processed review
+  this.constructor.Review.calcAvarageRatings(this.tour)
+});
 
 const Review = mongoose.model('Review,', reviewSchema);
+
 module.exports = Review;
