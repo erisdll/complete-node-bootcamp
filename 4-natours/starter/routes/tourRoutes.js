63dc4a4962395207ea47d16a0d8943ea4c0a282d
erisdll <72031082+erisdll@users.noreply.github.com>
2023-10-13 21:59:48 -0300

    Updated tourRouter.js

--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -21,6 +21,12 @@ router
   );
 
 router
+  .route('/tours-within/:distance/center/:latlng/unit/:unit')
+  .get(tourController.getToursWith);
+
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances)
+
+router
   .route('/')
   .get(tourController.getAllTours)
   .post(
