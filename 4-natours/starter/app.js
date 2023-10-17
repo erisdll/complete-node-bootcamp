393b3da45c3baa3d48021c02532d37ab2c6860d2
nearlyerika <72031082+nearlyerika@users.noreply.github.com>
2023-10-17 16:50:07 -0300

    Updated App to enable  views

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -11,6 +11,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes')
 
 const app = express();
 
@@ -69,13 +70,8 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Erika'
-  });
-});
 
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
