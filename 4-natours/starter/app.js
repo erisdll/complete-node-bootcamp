5992280ec341bc66a94e17a94bdf99b5af73c2f7
erisdll <72031082+erisdll@users.noreply.github.com>
2023-10-19 19:43:14 -0300

    b

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -13,6 +14,9 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'))
 
 // 1) GLOBAL MIDDLEWARES
 // Set secure HTTP headers
@@ -55,7 +59,7 @@ app.use(
 );
 
 // Static file server
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Test middleware
 app.use((req, res, next) => {
