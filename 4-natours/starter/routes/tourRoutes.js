971222ceb559b08b75a900b4dd468a7d527d275a
erisdll <72031082+erisdll@users.noreply.github.com>
2023-10-19 22:58:32 -0300

    fixing bugs

--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -22,7 +22,7 @@ router
 
 router
   .route('/tours-within/:distance/center/:latlng/unit/:unit')
-  .get(tourController.getToursWith);
+  .get(tourController.getToursWithin);
 
 router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances)
 
