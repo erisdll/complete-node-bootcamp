0f169c3a383f50a45d059be691a1f8a5c812adef
nearlyerika <72031082+nearlyerika@users.noreply.github.com>
2023-10-30 16:41:48 -0300

    updated views controller

--- 4-natours/starter/controllers/viewsController.js
+++ 4-natours/starter/controllers/viewsController.js
@@ -1,5 +1,5 @@
-const Tour = require('../models/tourModel')
-const catchAsync = require('../utils/catchAsync')
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
 
 exports.getOverview = catchAsync(async (req, res) => {
   // 1) Get tour data from collection
@@ -9,20 +9,20 @@ exports.getOverview = catchAsync(async (req, res) => {
   // 3) Render template using data from step 1
   res.status(200).render('overview', {
     title: 'All Tours',
-    tours: tours
+    tours: tours,
   });
 });
 
 exports.getTour = catchAsync(async (req, res) => {
   // 1) Get the datea for the requested tour (including reviews and guides)
-  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+  const tour = await Tour.findOne({slug: req.params.slug}).populate({
     path: 'reviews',
-    fields: 'review rating user'
+    fields: 'review rating user',
   });
   // 2) Build template
 
   // 3) Render template using data  from step 1
   res.status(200).render('tour', {
-    tour: 'The Forest Hiker Tour'
+    tour: `${tour.name} tour`,
   });
 });
